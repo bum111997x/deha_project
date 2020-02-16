5ff7c3a8e93324e16db277a17a7991665d172093
bum111997x <bum111997x@gmail.com>
2020-02-16 21:43:46 +0700

    create permission database

---
+++ app/Permission.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    public $timestamps = true;
+    protected $table = 'permissions';
+    protected $fillable = [
+        'permission_name',
+        'permission_label',
+    ];
+
+//    public function roles()
+//    {
+//        return $this->belongsToMany('App\Role', 'permission_role', 'permission_id', 'role_id');
+//    }
+//
+//    public function getAll()
+//    {
+//        return $this->all();
+//    }
+}
